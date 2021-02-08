5781fd4aea867669c3f7d3eb99a26975a349970c
francine babauta <francine.babauta@outlook.com>
2021-02-07 23:22:58 -0800

    added model folder for html layouts added routes

---
+++ routes/api-routes.js
@@ -0,0 +1,68 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// get all workout data
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(error);
+        });
+});
+
+// get last workout 
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .sort({ day: -1 })
+        .limit(1)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+// add new workout
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } },
+        { upsert: true, "new": true })
+        .then(data => {
+            let duration = data.calcDuration();
+            db.Workout.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: { totalDuration: duration } },
+            )
+                .then(updated => {
+                    res.json(updated);
+                })
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+module.exports = router;
+    
+})
