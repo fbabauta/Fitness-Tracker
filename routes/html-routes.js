5c22a6370e079115edd09513725c892bbbe5fbcf
francine babauta <francine.babauta@outlook.com>
2021-02-08 12:03:39 -0800

    added html layouts

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+module.exports = router;
