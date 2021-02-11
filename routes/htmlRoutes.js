ce5bfa38579e68df04eb5566f1fa141664a04342
francine babauta <francine.babauta@outlook.com>
2021-02-11 03:17:37 -0800

    added htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,10 @@
 const path = require("path");
 
-
 module.exports = function (app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
+  // called when "Continue Workout" or "new Workout" is clicked in index.html
   app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-
   app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
