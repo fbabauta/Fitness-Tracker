71c75242d8fa2f722d7d47efc39bce8c4fde1248
francine babauta <francine.babauta@outlook.com>
2021-02-11 17:02:49 -0800

    updating server file to get all routes

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const db = require("./models");
 
+// set up port and express app
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+// set up logger
 app.use(logger("dev"));
+
+// set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// set up mongodb connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", { useNewUrlParser: true });
 
-const db = require("./models");
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
