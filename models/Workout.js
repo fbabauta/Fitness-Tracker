9011d694d867747bf923b7281d8b18e3cdb1d751
francine babauta <francine.babauta@outlook.com>
2021-02-11 16:33:24 -0800

    fixed minor changes

--- models/Workout.js
+++ models/Workout.js
@@ -1,24 +1,52 @@
 const mongoose = require("mongoose");
-const moment = require("moment");
+
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    required: true,
-    // default: moment().format("YYYY-MM-DD HH:mm")
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
   },
-  dateTime: {
-    type: Number,
-    required: true,
-  },
-  totalDuration: {
-    type: Number,
-    default: 0,
-  },
-  exercises: []
-});
-
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+  }
+);
+  
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
