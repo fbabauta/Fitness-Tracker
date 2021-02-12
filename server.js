a6a67407ab7b1cb9b3843fd1fbf4437961c0969b
francine babauta <francine.babauta@outlook.com>
2021-02-12 02:13:12 -0800

    restructuring server and seeder for proper requirements

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const db = require("./models");
 
-// set up port and express app
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-// set up logger
 app.use(logger("dev"));
-
-// set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// set up mongodb connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// require routes
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
