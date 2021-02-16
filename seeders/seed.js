0716c351c0733bf219eb47c3aa114b58958de3af
francine babauta <francine.babauta@outlook.com>
2021-02-16 04:36:08 -0800

    deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,15 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(
+   process.env.MONGODB_URI ||'mongodb://localhost/workout',
+  {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+  }
+);
 
 let workoutSeed = [
   {
