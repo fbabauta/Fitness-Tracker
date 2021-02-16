9af30c2f6dc149e079f746d38b4298296c67e12b
francine babauta <francine.babauta@outlook.com>
2021-02-16 15:50:19 -0800

    deploying

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
+  
+require("dotenv");
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+var compression = require('compression');
 
-require("./models");
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
+const workout = require("./models");
 
 const app = express();
-
-app.use(morgan("dev"));
+app.use(compression({ filter: shouldCompress }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+//var MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+
+//code stars here
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
+    console.log(`App running on port ${PORT}!`);
+  });
