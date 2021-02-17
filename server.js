9d8a0041634a1863109dd5d9250201cacded7e15
francine babauta <francine.babauta@outlook.com>
2021-02-16 19:47:14 -0800

    revised routes on server file

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes"));
+app.use(require("./routes/api"));
+app.use(require("./routes/view"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
