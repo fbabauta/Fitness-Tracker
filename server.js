19cdb991288a25a3d9baa5cf623cb0ebb98d5b10
francine babauta <francine.babauta@outlook.com>
2021-02-16 18:45:26 -0800

    deploying

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
 
@@ -13,7 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
