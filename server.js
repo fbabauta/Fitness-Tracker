0195aa7c2123a0c7d8c798b4fbd1786b42fb725f
francine babauta <francine.babauta@outlook.com>
2021-02-16 16:23:31 -0800

    changed routes

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
-  
-require("dotenv");
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-var compression = require('compression');
 
-const PORT = process.env.PORT || 3000;
-
-const workout = require("./models");
+const PORT = 3000;
 
 const app = express();
-app.use(compression({ filter: shouldCompress }));
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-//const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-function shouldCompress (req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false
-  }
- 
-  // fallback to standard filter function
-  return compression.filter(req, res)
-}
-
-//code stars here
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
